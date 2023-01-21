2a3ab53d36ca6d542ea3e3955a71e4c832e71b16
turtton <top.gear7509@turtton.net>
2023-01-21 13:06:03 +0900

    refactor: format codes

--- lib/obsidian-image.ts
+++ lib/obsidian-image.ts
@@ -1,30 +1,30 @@
-import {visit} from "unist-util-visit";
-import {Node} from "unist";
-import {Settings} from "unified";
-import {VFile} from "vfile";
+import { visit } from "unist-util-visit";
+import { Node } from "unist";
+import { Settings } from "unified";
+import { VFile } from "vfile";
 
 const regex = /!\[\[(([a-z\-_0-9\\/:]+\s*)+\.(jpg|jpeg|png|gif|svg|webp))]]/gi;
 const regex2 = /!\[\[(([a-z\-_0-9\\/:]+\s*)+\.(jpg|jpeg|png|gif|svg|webp))]]/gi; //TODO why can't I reuse regex literal???
 
 interface CustomNode extends Node {
-    type: "text-temp" | "text" | "image" | "paragraph"
-    children?: CustomNode[]
+  type: "text-temp" | "text" | "image" | "paragraph";
+  children?: CustomNode[];
 }
 
 interface ImageNode extends CustomNode {
-    type: "image"
-    url: string
-    alt: string
+  type: "image";
+  url: string;
+  alt: string;
 }
 
 interface TextNode extends CustomNode {
-    value: string
+  value: string;
 }
 
 function convertTextNode(node): CustomNode {
-    const searchText: string = node.value;
+  const searchText: string = node.value;
 
-    /*
+  /*
     This regex MATCH following type of image link
       ![[youtube.png]]    ==> Image with no folder path, no space in between
       ![[a .png]]         ==> with space
@@ -46,63 +46,65 @@ function convertTextNode(node): CustomNode {
       ![[/tuancao/a.md]] ==> Unsupported format
       ![[/tuancao/a.mp4]]==> Unsupported format
     */
-    const matches = searchText.matchAll(regex);
+  const matches = searchText.matchAll(regex);
 
-    let startIndex = 0;
-    let endIndex = searchText.length;
+  let startIndex = 0;
+  let endIndex = searchText.length;
 
-    let children: CustomNode[] = [];
-    for (const match of matches) {
-        endIndex = match.index;
+  let children: CustomNode[] = [];
+  for (const match of matches) {
+    endIndex = match.index;
 
-        // Constructing text node from un-matched string
-        // const textNode: TextNode = {
-        //     // change type child node, so that visit() function won't recursively visit this node with "text" type
-        //     type: "text-temp",
-        //     value: searchText.substring(startIndex, endIndex),
-        // };
+    // Constructing text node from un-matched string
+    // const textNode: TextNode = {
+    //     // change type child node, so that visit() function won't recursively visit this node with "text" type
+    //     type: "text-temp",
+    //     value: searchText.substring(startIndex, endIndex),
+    // };
 
-        const imageNode: ImageNode = {
-            type: "image",
-            //TODO: Use some kind of option to pass in default images path
-            url: encodeURI(`/images/${match[1]}`), //encode white space from file name
-            alt: match[1],
-        };
-
-        children.push(imageNode);
-
-        startIndex = match.index + match[0].length;
-    }
-
-    if (startIndex < searchText.length) {
-        const textNode: TextNode = {
-            type: "text-temp",
-            value: searchText.substring(startIndex, searchText.length),
-        };
-        children.push(textNode);
-    }
-
-    return {
-        type: "paragraph",
-        children: children,
+    const imageNode: ImageNode = {
+      type: "image",
+      //TODO: Use some kind of option to pass in default images path
+      url: encodeURI(`/images/${match[1]}`), //encode white space from file name
+      alt: match[1],
     };
+
+    children.push(imageNode);
+
+    startIndex = match.index + match[0].length;
+  }
+
+  if (startIndex < searchText.length) {
+    const textNode: TextNode = {
+      type: "text-temp",
+      value: searchText.substring(startIndex, searchText.length),
+    };
+    children.push(textNode);
+  }
+
+  return {
+    type: "paragraph",
+    children: children,
+  };
 }
 
-export default function attacher(options?: Settings): (tree: CustomNode, vfile: VFile) => CustomNode {
-    return function transformer(tree: CustomNode, vfile: VFile) {
-        visit(tree, "text", (node: TextNode) => {
-            if (regex2.test(node.value)) {
-                const newNode = convertTextNode(node);
-                node.type = "paragraph";
-                node.children = newNode.children;
-            }
-        });
+export default function attacher(
+  options?: Settings
+): (tree: CustomNode, vfile: VFile) => CustomNode {
+  return function transformer(tree: CustomNode, vfile: VFile) {
+    visit(tree, "text", (node: TextNode) => {
+      if (regex2.test(node.value)) {
+        const newNode = convertTextNode(node);
+        node.type = "paragraph";
+        node.children = newNode.children;
+      }
+    });
 
-        // Change back "text-temp" node ==> "text" to clean up
-        visit(tree, "text-temp", (node: TextNode) => {
-            node.type = "text";
-        });
+    // Change back "text-temp" node ==> "text" to clean up
+    visit(tree, "text-temp", (node: TextNode) => {
+      node.type = "text";
+    });
 
-        return tree;
-    };
+    return tree;
+  };
 }
