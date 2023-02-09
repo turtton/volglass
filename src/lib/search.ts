43abf6c8c3c4d59b1d024d6b51fbaa10c1702791
turtton <top.gear7509@turtton.net>
2023-02-09 13:59:44 +0900

    feat: add search index creation process

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,3 +1,10 @@
+import { getAllMarkdownFiles, isFile } from "./io";
+import path from "path";
+import fs from "fs";
+import { Transformer } from "./transformer";
+import { isJapanese, toRomaji } from "wanakana";
+import { getRouterPath } from "./slug";
+
 export interface SearchData {
   title: string;
   rawTitle: string;
@@ -5,4 +12,48 @@ export interface SearchData {
   rawContent: string;
   lineAt: number;
   path: string;
+}
+
+export function getSearchIndex(): SearchData[] {
+  const filePath = path.join(process.cwd(), "search-index.json");
+  if (fs.existsSync(filePath) && isFile(filePath)) {
+    const rawData = fs.readFileSync(filePath);
+    return JSON.parse(rawData.toString());
+  } else {
+    const result: SearchData[] = []
+    try {
+      fs.rmSync(filePath);
+    } catch (ignore) {
+    }
+    const filePaths = getAllMarkdownFiles();
+    filePaths.forEach((markdownFile) => {
+      const title = Transformer.parseFileNameFromPath(markdownFile);
+      if (title == null) {
+        return;
+      }
+      const rawTitle = isJapanese(title) ? toRomaji(title) : title;
+      const content = fs.readFileSync(markdownFile)
+      if (content === null || content === undefined) {
+        return;
+      }
+      const path = getRouterPath(`${title}.md`)
+      if (path === null) { return; }
+      content.toString()
+        .split('\n')
+        .forEach((line, index) => {
+          if (line.match("```") !== null || line.match("---") !== null) return;
+          const rawContent = isJapanese(line) ? toRomaji(line) : line
+          result.push({
+            title,
+            rawTitle,
+            singleLineContent: line,
+            rawContent,
+            lineAt: index,
+            path
+          })
+        })
+    });
+    fs.writeFileSync(filePath, JSON.stringify(result), "utf-8");
+    return result
+  }
 }
