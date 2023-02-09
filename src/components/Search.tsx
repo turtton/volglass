56923c56cf43507b0ad933b4530ce78a1e23291d
turtton <top.gear7509@turtton.net>
2023-02-09 14:01:07 +0900

    style: fix result text align

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -59,43 +59,41 @@ export function SearchBar(prop: SearchProp): JSX.Element {
 
 function ResultList({ contents, hidden }: ResultProp): JSX.Element {
   const router = useRouter();
-  switch (contents) {
-    case "Empty":
-      return <div hidden={true} />;
-    default:
-      return (
-        <div
-          className="absolute z-10 mt-2 w-11/12 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
-          hidden={hidden}
-        >
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-            {contents.length === 0 ? (
-              <li className="inline-flex w-full px-4 py-2">
-                <p>Not Found</p>
-              </li>
-            ) : (
-              contents.map((data) => (
-                <li
-                  key={data.rawTitle}
-                  className="inline-flex w-full px-4 py-2 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  <button
-                    className="truncate"
-                    onClick={(): void => {
-                      void router.push(data.path);
-                    }}
-                    key={data.title}
-                  >
-                    <p className="w-full truncate text-left underline">{data.title}</p>
-                    <p className="truncate text-xs text-gray-500">
-                      {data.lineAt}: {data.singleLineContent}
-                    </p>
-                  </button>
-                </li>
-              ))
-            )}
-          </ul>
-        </div>
-      );
+  if (contents === "Empty") {
+    return <div hidden={true} />;
   }
+  return (
+    <div
+      className="absolute z-10 mt-2 w-11/12 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+      hidden={hidden}
+    >
+      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+        {contents.length === 0 ? (
+            <li className="inline-flex w-full px-4 py-2">
+              <p>Not Found</p>
+            </li>
+          ) :
+          contents.map((data) => (
+            <li
+              key={data.rawTitle}
+              className="inline-flex w-full px-4 py-2 dark:hover:bg-gray-600 dark:hover:text-white"
+              onClick={(): void => {
+                void router.push(data.path);
+              }}
+            >
+              <button
+                className="w-full truncate"
+                key={data.title}
+              >
+                <p className="w-full truncate text-left underline">{data.title}</p>
+                <p className="truncate text-left text-xs text-gray-500">
+                  {data.lineAt}: {data.singleLineContent}
+                </p>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 }
