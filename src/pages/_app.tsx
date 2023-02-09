771fe3e698baffd527ee7ef42a7ee1025e4b9cc9
turtton <top.gear7509@turtton.net>
2023-02-09 11:14:36 +0900

    refactor: remove unneeded code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../styles/global.css";
 import "../styles/style.css";
 import "../styles/prism.css";
-import PlausibleProvider from "next-plausible";
 import { AppProps } from "next/app";
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <PlausibleProvider domain="mindstone.tuancao.me">
-      <Component {...pageProps} />
-    </PlausibleProvider>
+    <Component {...pageProps} />
   );
 }
