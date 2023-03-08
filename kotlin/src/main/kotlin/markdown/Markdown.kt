8a6004b3599347699a2ee23272ea994a3bca2bce
turtton <top.gear7509@turtton.net>
2023-03-08 20:08:01 +0900

    feat: export markdown parsers

--- kotlin/src/main/kotlin/markdown/Markdown.kt
+++ kotlin/src/main/kotlin/markdown/Markdown.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalJsExport::class)
+
 package markdown
 
 import markdown.processor.element.createReactElementGeneratingProcessors
@@ -7,12 +9,14 @@ import org.intellij.markdown.parser.MarkdownParser
 import react.FC
 import react.Props
 
+@JsExport
 fun convertMarkdownToReactElement(content: String): FC<Props> {
     val flavour = ObsidianMarkFlavourDescriptor()
     val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(content)
     return ReactElementGenerator(content, parsedTree, createReactElementGeneratingProcessors()).generateElement()
 }
 
+@JsExport
 fun convertMarkdownToHtml(content: String): String {
     val flavour = CommonMarkFlavourDescriptor()
     val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(content)
