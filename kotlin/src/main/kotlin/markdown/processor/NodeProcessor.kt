59c6b1dc4977c994aa7a0a0c981a14decb08a1de
turtton <top.gear7509@turtton.net>
2023-03-08 14:37:43 +0900

    feat: add NodeProcessor

---
+++ kotlin/src/main/kotlin/markdown/processor/NodeProcessor.kt
@@ -0,0 +1,20 @@
+package markdown.processor
+
+import markdown.TagConsumer
+import org.intellij.markdown.ast.ASTNode
+import org.intellij.markdown.ast.acceptChildren
+
+interface NodeProcessor<Tag, Parent> {
+    fun<Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : org.intellij.markdown.ast.visitors.Visitor
+}
+
+interface SandwichNodeProcessor<Tag, Parent> : NodeProcessor<Tag, Parent> {
+    fun openTag(visitor: TagConsumer<Tag, Parent>, markdownText: String, node: ASTNode)
+    fun closeTag(visitor: TagConsumer<Tag, Parent>, markdownText: String, node: ASTNode)
+
+    override fun <Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : org.intellij.markdown.ast.visitors.Visitor {
+        openTag(visitor, markdownText, node)
+        node.acceptChildren(visitor)
+        closeTag(visitor, markdownText, node)
+    }
+}
