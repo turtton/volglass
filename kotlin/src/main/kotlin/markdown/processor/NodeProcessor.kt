10809cfbb222401ff58a2ebada30249dc16e4837
turtton <top.gear7509@turtton.net>
2023-03-09 11:16:12 +0900

    style: rename target visitor class

--- kotlin/src/main/kotlin/markdown/processor/NodeProcessor.kt
+++ kotlin/src/main/kotlin/markdown/processor/NodeProcessor.kt
@@ -3,16 +3,17 @@ package markdown.processor
 import markdown.TagConsumer
 import org.intellij.markdown.ast.ASTNode
 import org.intellij.markdown.ast.acceptChildren
+import org.intellij.markdown.ast.visitors.Visitor as NodeVisitor
 
 interface NodeProcessor<out Tag, in Parent> {
-    fun <Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : org.intellij.markdown.ast.visitors.Visitor
+    fun <Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : NodeVisitor
 }
 
 interface SandwichNodeProcessor<out Tag, in Parent> : NodeProcessor<Tag, Parent> {
     fun openTag(visitor: TagConsumer<Tag, Parent>, markdownText: String, node: ASTNode)
     fun closeTag(visitor: TagConsumer<Tag, Parent>, markdownText: String, node: ASTNode)
 
-    override fun <Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : org.intellij.markdown.ast.visitors.Visitor {
+    override fun <Visitor> processNode(visitor: Visitor, markdownText: String, node: ASTNode) where Visitor : TagConsumer<Tag, Parent>, Visitor : NodeVisitor {
         openTag(visitor, markdownText, node)
         node.acceptChildren(visitor)
         closeTag(visitor, markdownText, node)
