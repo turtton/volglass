cf30fe415da15a8cbe9854909b8091f30f946284
turtton <top.gear7509@turtton.net>
2023-03-12 18:41:28 +0900

    fix: add missed args

--- kotlin/src/main/kotlin/markdown/Markdown.kt
+++ kotlin/src/main/kotlin/markdown/Markdown.kt
@@ -5,6 +5,7 @@ package markdown
 import markdown.processor.element.createReactElementGeneratingProcessors
 import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.LinkMap
 import org.intellij.markdown.parser.MarkdownParser
 import react.FC
 import react.Props
@@ -13,7 +14,7 @@ import react.Props
 fun convertMarkdownToReactElement(content: String): FC<Props> {
     val flavour = ObsidianMarkFlavourDescriptor()
     val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(content)
-    return ReactElementGenerator(content, parsedTree, createReactElementGeneratingProcessors()).generateElement()
+    return ReactElementGenerator(content, parsedTree, createReactElementGeneratingProcessors(LinkMap(hashMapOf()), null)).generateElement()
 }
 
 @JsExport
