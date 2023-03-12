aedb4b95c76d0533fe8a0b39786ec3c43b1cfc00
turtton <top.gear7509@turtton.net>
2023-03-12 18:41:49 +0900

    chore: add kotlin-react-test library

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("js") version "1.8.0"
+    kotlin("js") version "1.8.10"
     id("org.jmailen.kotlinter") version "3.13.0"
 }
 
@@ -8,10 +8,12 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://maven.turtton.net")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.github.mysticfall:kotlin-react-test:18.2.0-pre.346+build.17")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
