db3616e9519d99aa19fd0163bad30d472eeb747d
turtton <top.gear7509@turtton.net>
2023-03-13 16:10:43 +0900

    test: add useful functions

---
+++ kotlin/src/test/kotlin/markdown/Extensions.kt
@@ -0,0 +1,24 @@
+package markdown
+
+import kotlin.js.Json
+import kotlin.test.assertEquals
+import markdown.processor.element.createReactElementGeneratingProcessors
+import mysticfall.kotlin.react.test.ReactTestSupport
+import org.intellij.markdown.parser.LinkMap
+import org.intellij.markdown.parser.MarkdownParser
+import react.ChildrenBuilder
+import react.dom.html.ReactHTML
+
+private val descriptor = ObsidianMarkFlavourDescriptor()
+
+private val parser = MarkdownParser(descriptor)
+fun Json.serialize(): String = JSON.stringify(this)
+fun ReactTestSupport.doTest(markdown: String, expect: ChildrenBuilder.() -> Unit) {
+    val markdownTree = parser.buildMarkdownTreeFromString(markdown)
+    val element = ReactElementGenerator(markdown, markdownTree, createReactElementGeneratingProcessors(LinkMap(hashMapOf()), null)).generateElement()
+    val actualRender = render { element() }
+    val expectRender = render { ReactHTML.body(expect) }
+    val expectJson = expectRender.toJSON().serialize()
+    val actualJson = actualRender.toJSON().serialize()
+    assertEquals(expectJson, actualJson, "\nEx: $expectJson\nAc: $actualJson\n")
+}
