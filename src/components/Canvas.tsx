119c3264b09c72f78ce887eb512e1c06f8eb9bae
turtton <top.gear7509@turtton.net>
2023-03-28 13:01:57 +0900

    style: suppress lint error

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -17,66 +17,65 @@ import {
 } from "volglass-backend";
 import { match, P } from "ts-pattern";
 
-const Canvas =
-	(canvas: CanvasData, readContent: (id: string) => FC) => ({}) => {
-		const nodes = canvas.nodes.map(
-			(node): Node<LinkData> => ({
-				id: node.id,
-				position: { x: node.x, y: node.y },
-				// FIXME: not work
-				width: node.width,
-				height: node.height,
-				type: "link",
-				data: match(node.nodeData)
-					.with(P.instanceOf(NodeText), (nodeText) => ({
-						contentHtml: () => <p>{nodeText.text}</p>,
-						positions: findTargetSide(node.id, canvas.edges),
-						width: node.width,
-						height: node.height,
-					}))
-					.with(P.instanceOf(NodeFile), (nodeFile) => ({
-						slug: `/${nodeFile.file}`.replace(".md", ""),
-						contentHtml: readContent(`/${nodeFile.file}`),
-						positions: findTargetSide(node.id, canvas.edges),
-						width: node.width,
-						height: node.height,
-					}))
-					.run(),
-			}),
-		);
-		const edges = canvas.edges.map(
-			(edge): Edge => ({
-				id: edge.id,
-				source: edge.fromNode,
-				sourceHandle: convertSideData(edge.fromSide),
-				target: edge.toNode,
-				targetHandle: convertSideData(edge.toSide),
-			}),
-		);
-		const nodeTypes = useMemo(
-			() => ({
-				link: LinkNode,
-			}),
-			[],
-		);
-		return (
-			<div className="canvas">
-				{nodes && (
-					<ReactFlow
-						connectionLineType={ConnectionLineType.Bezier}
-						fitView
-						nodeTypes={nodeTypes}
-						nodes={nodes}
-						edges={edges}
-						minZoom={0.03}
-					>
-						<Background />
-						<Controls className="rounded-md dark:bg-dark-background-primary dark:border-gray-600 dark:fill-gray-200" />
-					</ReactFlow>
-				)}
-			</div>
-		);
-	};
+const Canvas = (canvas: CanvasData, readContent: (id: string) => FC) => () => {
+	const nodes = canvas.nodes.map(
+		(node): Node<LinkData> => ({
+			id: node.id,
+			position: { x: node.x, y: node.y },
+			// FIXME: not work
+			width: node.width,
+			height: node.height,
+			type: "link",
+			data: match(node.nodeData)
+				.with(P.instanceOf(NodeText), (nodeText) => ({
+					contentHtml: () => <p>{nodeText.text}</p>,
+					positions: findTargetSide(node.id, canvas.edges),
+					width: node.width,
+					height: node.height,
+				}))
+				.with(P.instanceOf(NodeFile), (nodeFile) => ({
+					slug: `/${nodeFile.file}`.replace(".md", ""),
+					contentHtml: readContent(`/${nodeFile.file}`),
+					positions: findTargetSide(node.id, canvas.edges),
+					width: node.width,
+					height: node.height,
+				}))
+				.run(),
+		}),
+	);
+	const edges = canvas.edges.map(
+		(edge): Edge => ({
+			id: edge.id,
+			source: edge.fromNode,
+			sourceHandle: convertSideData(edge.fromSide),
+			target: edge.toNode,
+			targetHandle: convertSideData(edge.toSide),
+		}),
+	);
+	const nodeTypes = useMemo(
+		() => ({
+			link: LinkNode,
+		}),
+		[],
+	);
+	return (
+		<div className="canvas">
+			{nodes && (
+				<ReactFlow
+					connectionLineType={ConnectionLineType.Bezier}
+					fitView
+					nodeTypes={nodeTypes}
+					nodes={nodes}
+					edges={edges}
+					minZoom={0.03}
+				>
+					<Background />
+					<Controls className="rounded-md dark:bg-dark-background-primary dark:border-gray-600 dark:fill-gray-200" />
+				</ReactFlow>
+			)}
+		</div>
+	);
+};
 
 const convertSideData = (side: NodeSide) =>
 	match(side.name)
