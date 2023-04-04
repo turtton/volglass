c6624525444f034c05c7416775c46e7456db3557
turtton <top.gear7509@turtton.net>
2023-04-04 15:27:57 +0900

    revert: rollback kotlin
    Since kotlin 1.8.20 requires node-v18, compatibility with CloudflarePages has been lost.

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("multiplatform") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
     id("org.jmailen.kotlinter") version "3.14.0"
 }
 
